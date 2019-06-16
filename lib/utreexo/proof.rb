5de8e5ea1719be7014102ee61eef2b282a602065
azuchi <azuchi@chaintope.com>
2019-06-16 09:26:44 +0900

    Add comment

--- lib/utreexo/proof.rb
+++ lib/utreexo/proof.rb
@@ -5,16 +5,24 @@ module Utreexo
     attr_reader :payload  # hash of the thing itself (what's getting proved)
     attr_reader :siblings # hash of siblings up to a root
 
+    # initialize
+    # @param [Boolean] right Whether the element is a node on the right
+    # @param [String] payload Target element
+    # @param [Array[String]] siblings proofs
     def initialize(right, payload, siblings = [])
       @right = right
       @payload = payload
       @siblings = siblings
     end
 
+    # Whether the element is a node on the right
+    # @return [Boolean]
     def right?
       right
     end
 
+    # Whether the element is a node on the left
+    # @return [Boolean]
     def left?
       !right
     end
